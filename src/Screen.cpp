56ff0a17418239b46aeb5c44cfa8c18161246c2d
zackmon99 <monzack4@gmail.com>
2020-06-18 11:53:34 -0700

    Initial commit

---
+++ src/Screen.cpp
@@ -0,0 +1,154 @@
+#include "Screen.hpp"
+
+namespace zmgfx
+{
+Screen::Screen(): 
+    m_window(NULL), m_renderer(NULL), m_texture(NULL), m_buffer1(NULL), m_buffer2(NULL)
+{
+
+}
+bool Screen::init(const char * windowName)
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL Init failed." << std::endl;
+        return false;
+    }
+    std::cout << "SDL Init succeeded." << std::endl;
+
+    m_window = SDL_CreateWindow(windowName, 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+    if ( m_window == NULL )
+    {
+        std::cout << "Failed to create SDL window:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return false;
+    }
+
+    std::cout << "SDL window created." << std::endl;
+
+    m_renderer = SDL_CreateRenderer( m_window, -1, SDL_RENDERER_PRESENTVSYNC );
+    m_texture = SDL_CreateTexture( m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT );
+
+    if( m_renderer == NULL )
+    {
+        std::cout << "Could not create renderer." << std::endl;
+        SDL_DestroyWindow(m_window);
+        SDL_Quit();
+        return false;
+    }
+
+    if ( m_texture == NULL )
+    {
+        std::cout << "Could not create texture." << std::endl;
+        SDL_DestroyRenderer(m_renderer);
+        SDL_DestroyWindow(m_window);
+        SDL_Quit();
+        return false;
+    }
+    
+    m_buffer1 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+    m_buffer2 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    memset(m_buffer1, 0x00, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+    memset(m_buffer2, 0x00, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+    return true;
+}
+bool Screen::processEvents()
+{
+    while( SDL_PollEvent(&m_event) )
+        {
+            if( m_event.type == SDL_QUIT )
+            {
+                return false;
+            }
+        }
+    return true;
+}
+
+void Screen::setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue)
+{
+    if (x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT )
+    {
+        return;
+    }
+    Uint32 color = 0;
+    color += red;
+    color <<= 8;
+    color += green;
+    color <<= 8;
+    color += blue;
+    color <<= 8;
+    color += 0xFF;
+
+    m_buffer1[(y * SCREEN_WIDTH) + x] = color;
+    ;
+}
+
+void Screen::update()
+{
+    SDL_UpdateTexture( m_texture, NULL, m_buffer1, SCREEN_WIDTH * sizeof(Uint32) );
+    SDL_RenderClear( m_renderer );
+    SDL_RenderCopy( m_renderer, m_texture, NULL, NULL );
+    SDL_RenderPresent( m_renderer );
+
+}
+
+void Screen::clear()
+{
+    memset(m_buffer1, 0x00, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+    memset(m_buffer2, 0x00, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+}
+
+void Screen::boxBlur()
+{
+    Uint32 *temp = m_buffer1;
+    m_buffer1 = m_buffer2;
+    m_buffer2 = temp;
+
+    for ( int y = 0; y < SCREEN_HEIGHT; y++ )
+    {
+        for ( int x = 0; x < SCREEN_WIDTH; x++ )
+        {
+            int redTotal = 0;
+            int greenTotal = 0;
+            int blueTotal = 0;
+            for( int row = -1 ; row <= 1; row ++ )
+            {
+                for ( int col = -1; col <= 1; col++ )
+                {
+                    int currentX = x + col;
+                    int currentY = y + row;
+
+                    if ( currentX >=0 && currentX < SCREEN_WIDTH 
+                        && currentY >= 0 && currentY < SCREEN_HEIGHT )
+                    {
+                        Uint32 color = m_buffer2[currentY * SCREEN_WIDTH + currentX];
+                        Uint8 red = color >> 24;
+                        Uint8 green = color  >> 16;
+                        Uint8 blue = color >> 8;
+
+                        redTotal += red;
+                        greenTotal += green;
+                        blueTotal += blue;
+                    }
+                }
+            }
+            Uint8 red = redTotal / 9;
+            Uint8 green = greenTotal / 9;
+            Uint8 blue = blueTotal /9;
+            setPixel( x, y, red, green, blue );
+        }
+    }
+}
+
+void Screen::close()
+{
+    delete [] m_buffer1;
+    delete [] m_buffer2;
+    SDL_DestroyTexture( m_texture );
+    SDL_DestroyRenderer( m_renderer );
+    SDL_DestroyWindow( m_window );
+    SDL_Quit();
+}
+}
